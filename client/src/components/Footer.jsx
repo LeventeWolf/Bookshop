e51ffba39f069f1562d494e88f445f786b10a074
Kredam <akitlosz2000@gmail.com>
2022-02-11 19:23:25 +0100

    UserAPi

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import { Box, Stack } from "@chakra-ui/react";
+import {faAt} from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
@@ -8,6 +12,14 @@ function Footer() {
           <p className="m-0 text-center text-white">
             Follow Us
           </p>
+        <Stack>
+            <Box w={5} h={5}>
+                <FontAwesomeIcon color="white" icon={ faTwitter } />
+                <FontAwesomeIcon color="white" icon={ faFacebook } />
+                <FontAwesomeIcon color="white" icon={ faInstagram } />
+                <FontAwesomeIcon color="white" icon={ faAt } />
+            </Box>
+        </Stack>
         </div>
       </footer>
     </div>
