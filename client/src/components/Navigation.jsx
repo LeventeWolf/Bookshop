d896a59f21209eb1e1357b15f0ca777991b85d1b
LeventeWolf <wlevi2002@gmail.com>
2022-02-11 10:03:09 +0100

    Fix nav-icon issue

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -8,19 +8,19 @@ function Navigation() {
         <div className="nav-wrap">
             <div className="nav-primary-wrap">
                 <NavLink className="nav-home" to="/">
-                    <img className="nav-watchlist-home" src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
+                    <img className="nav-icon" src="https://cdn-icons-png.flaticon.com/512/25/25694.png"
                          alt="H" />
                     Home
                 </NavLink>
 
                 <NavLink className="nav-watchlist" to="/watchlist">
-                    <img className="nav-watchlist-heart" src="https://www.iconpacks.net/icons/1/free-heart-icon-492-thumb.png"
+                    <img className="nav-icon" src="https://www.iconpacks.net/icons/1/free-heart-icon-492-thumb.png"
                          alt="<3" />
                     <span>Wishlist</span>
                 </NavLink>
 
                 <NavLink className="nav-join" to="/join">
-                    <img className="nav-watchlist-heart" src="http://cdn.onlinewebfonts.com/svg/img_24787.png"
+                    <img className="nav-icon" src="http://cdn.onlinewebfonts.com/svg/img_24787.png"
                          alt="I" />
                     Sign in/Join
                 </NavLink>
