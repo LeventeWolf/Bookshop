b8b83dcc0854ef49c0db860d275c7f6d66534a85
Kredam <akitlosz2000@gmail.com>
2022-02-12 22:39:29 +0100

    created profile component

---
+++ client/src/components/user/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useSelector} from "react-redux";
+import { Box, Input } from "@chakra-ui/react";
+// import { getUserData } from './UserApi'
+import apiClient from '../../http-common'
+function Profile(){
+
+    return (
+        <Box>
+            Működök
+        </Box>
+    )
+
+}
+
+export default Profile
