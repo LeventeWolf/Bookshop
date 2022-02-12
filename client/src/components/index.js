fe1795f9ef2059475b04bbe74887f756bf1af9e8
Kredam <akitlosz2000@gmail.com>
2022-02-12 22:36:23 +0100

    added profile to exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,6 +3,7 @@ export { default as Header } from "./Header";
 export { default as HeaderSecondary } from "./HeaderSecondary";
 export { default as Footer } from "./Footer";
 export { default as Join } from "./user/Join";
+export { default as Profile } from "./user/Profile";
 export { default as Home } from "./home/Home";
 export { default as Basket } from "./basket/Basket";
 export { default as Wishlist } from "./wishlist/Wishlist";
