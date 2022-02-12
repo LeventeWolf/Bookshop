adb3ed62705a47e9d83fe772bcd1648af1feaf97
LeventeWolf <wlevi2002@gmail.com>
2022-02-12 18:46:15 +0100

    Update Product-Page

--- client/src/components/home/Bestsellers.jsx
+++ client/src/components/home/Bestsellers.jsx
@@ -12,9 +12,13 @@ export default function Bestsellers() {
     for (let i = 1; i <= 5; i++) {
         const product = {
             title: 'B-book #' + i,
+            longTitle: 'B-book #' + i + ' : A Handbook of Agile Software Craftsmanship',
+            description: 'Even bad code can function. But if code isn\'t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn\'t have to be that way.',
             author: faker.name.findName(),
             price: Math.floor(Math.random() * 20000) + 100,
-            imageUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/1323/9780132350884.jpg',
+            imageUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg',
+            type: 'Paperback',
+            language: 'English',
             quantity: 1,
         }
 
--- client/src/components/productPage/ProductPage.jsx
+++ client/src/components/productPage/ProductPage.jsx
@@ -1,31 +1,11 @@
 import React from "react";
 
 import {useSelector} from "react-redux";
-import {
-    BasketButton,
-    DescriptionContainer,
-    ProductAuthor,
-    ProductContainer,
-    ProductImage,
-    ProductPrice,
-    ProductTitle
-} from "../Component.styles";
 import Product from "../home/Product";
 
 
 export default function ProductPage() {
-    const product2 = useSelector(state => state.product);
-
-    const product = {
-        title: 'Clean Code',
-        longTitle: 'Clean Code : A Handbook of Agile Software Craftsmanship',
-        description: 'Even bad code can function. But if code isn\'t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn\'t have to be that way.',
-        imageUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg',
-        author: 'Robert Martin',
-        price: 16500,
-        type: 'Paperback',
-        language: 'English'
-    }
+    const product = useSelector(state => state.product);
 
     return (
         <div className="main-wrap">
