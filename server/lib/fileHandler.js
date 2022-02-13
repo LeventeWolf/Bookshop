6694f0f562f98119f8329e559c28831c343cef52
Kredam <akitlosz2000@gmail.com>
2022-02-13 12:55:42 +0100

    extended user login testing

--- server/lib/fileHandler.js
+++ server/lib/fileHandler.js
@@ -7,4 +7,15 @@ function getAllBooks(){
     return result;
 }
 
-module.exports = {getAllBooks}
+function authenticateUser(name, password){
+    const rawdata = fs.readFileSync('./database/users.json')
+    const result = JSON.parse(rawdata)
+    for (const user of result) {
+        if(user.name === name && user.password === password && name === undefined && password===undefined){
+            return true
+        }
+    }
+    return false
+}
+
+module.exports = {getAllBooks, authenticateUser}
