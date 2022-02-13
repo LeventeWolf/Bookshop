cc170ac2c061330f74baa743e934f01a51122577
LeventeWolf <wlevi2002@gmail.com>
2022-02-13 13:05:31 +0100

    Fix merge issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {Navigation, Header, HeaderSecondary, Footer, Home, Join, Basket, Wishlist, ProductPage, Profile} from "./components";
 import {ChakraProvider} from '@chakra-ui/react'
-
 import {Provider} from 'react-redux';
-import store from './redux/store'
 import {QueryClient, QueryClientProvider} from "react-query";
 import {ReactQueryDevtools} from "react-query/devtools";
+import store from './redux/store'
+import App from "./App";
 
-document.body.style.backgroundColor = '#F0F0F0';
 
 const queryClient = new QueryClient({
     defaultOptions:{
@@ -25,27 +21,12 @@ const queryClient = new QueryClient({
 
 ReactDOM.render(
     <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools />
-    <Provider store={store}>
-        <ChakraProvider>
-            <Router>
-                <Navigation/>
-                <Header/>
-                <HeaderSecondary/>
-
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/Join" element={<Join/>}/>
-                    <Route path="/wishlist" element={<Wishlist/>}/>
-                    <Route path="/basket" element={<Basket/>}/>
-                    <Route path="/product/:productName" element={<ProductPage/>}/>
-                    <Route path="/Profile" element={<Profile/>}/>
-                </Routes>
-
-                <Footer/>
-            </Router>
-        </ChakraProvider>
-    </Provider>
+        <ReactQueryDevtools />
+        <Provider store={store}>
+            <ChakraProvider>
+               <App />
+            </ChakraProvider>
+        </Provider>
     </QueryClientProvider>,
 
 
