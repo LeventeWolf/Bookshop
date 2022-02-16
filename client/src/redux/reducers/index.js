97692e21fb0195b47ec6ea9d301aafd70b9549a5
LeventeWolf <wlevi2002@gmail.com>
2022-02-16 16:05:08 +0100

    Remove unused counterReducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
-import { counterReducer } from "./counterReducer";
 import { basketReducer } from "./basketReducer";
 import { productReducer } from "./productReducer";
 
 const reducers = combineReducers({
   user: userReducer,
-  counter: counterReducer,
   basket: basketReducer,
   product: productReducer,
 });
